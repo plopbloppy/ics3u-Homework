96937040bb448dbb298de04bce9db7821bd25281
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-05 07:14:28 -0500

    Update CosineLaw.java

--- CosineLaw.java
+++ CosineLaw.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CosineLaw {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
+        
         System.out.println("Please enter a value for side a:");
         double a = keyboard.nextDouble();
         System.out.println("Please enter a value for side b:");
