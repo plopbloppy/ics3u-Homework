2f8de0ef311ffafff5994834db01333f548491f1
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-06 00:31:22 -0500

    Add files via upload

--- PolynomialEvaluator2.java
+++ PolynomialEvaluator2.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 
-import java.util.StringTokenizer;
-
-public class App {
+public class PolynomialEvaluator2 {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Please enter all coefficients, each separated by a space:");
+        System.out.println("Please enter the coefficient of each term separated by a space:");
         String coeffInput = keyboard.nextLine();
-        StringTokenizer coefficients = new StringTokenizer(coeffInput);
+        String[] coeffData = coeffInput.split(" ");
 
-        while (coefficients.hasMoreTokens()) {
-            String[] coefficient = new String[coefficients.countTokens()];
-            for (int i = 0; i < coefficients.countTokens(); i++) {
-                System.out.println(coefficient[i]);
-            }
+        System.out.println("Please enter the degree of each term separated by a space:");
+        String degInput = keyboard.nextLine();
+        String[] degData = degInput.split(" ");
+
+        System.out.println("Please enter an x value:");
+        double x = keyboard.nextDouble();
+        double fx = 0.0;
+        keyboard.close();
+
+        System.out.println("Your polynomial function is: f(x) = " );
+        for (int i = 0; i < coeffData.length; i++) {
+            fx += (Math.pow(Double.valueOf(coeffData[i]) * x, Double.valueOf(degData[i])));
+            System.out.println(coeffData[i] + "x^" + degData[i]);
         }
-        
-            
+        System.out.println("Your polynomial at x = " + x + " is: f(" + x + ") = " + fx);
     }
 }
