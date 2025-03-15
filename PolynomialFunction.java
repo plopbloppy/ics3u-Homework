2f1e704ae05f3ec820c890ff9778eb7c146c660f
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-15 12:50:23 -0400

    Add files via upload

--- PolynomialFunction.java
+++ PolynomialFunction.java
@@ -6,24 +6,23 @@ public class PolynomialFunction {
         String function = "f(x) = ";
         double sum = 0.0;
         
-        System.out.println("Please enter the coefficients, each separated by a space:");
+        System.out.println("Please enter the coefficient of each term separated by a space:");
         String coefficients = keyboard.nextLine();
-        System.out.println("Please enter the exponents, each separated by a space:");
+        System.out.println("Please enter the degree of each term separated by a space:");
         String exponents = keyboard.nextLine();
         System.out.println("Please enter an x value:");
         double x = keyboard.nextDouble();
         keyboard.close();
 
-        String[] coefData = coefficients.split(" ");
-        String[] expData = exponents.split(" ");  
+        String[] coeffSt = coefficients.split(" ");
+        String[] expSt = exponents.split(" ");  
 
-        for (int i = 0; i < coefData.length; i++) {
-            sum += (Math.pow(Double.parseDouble(coefData[i]) * x, Double.parseDouble(expData[i])));
-            if (i == coefData.length - 1) {
-                function += coefData[i] + "x^" + expData[i];
-            } else {
-                function += coefData[i] + "x^" + expData[i] + " + ";
-            }
+        for (int i = 0; i < coeffSt.length; i++) {
+            double coefficient = Double.parseDouble(coeffSt[i]);
+            int exponent = Integer.parseInt(expSt[i]);
+
+            function += (coefficient > 0 ? "+" : "") + coefficient + "x^" + exponent + " ";
+            sum += coefficient * Math.pow(x, exponent);
         }
         System.out.println("Your polynomial function is:" + function);
         System.out.println("Your polynomial at x = " + x + " is: f(" + x + ") = " + sum);
