8825ffb013169b9f51398d79daab7ea7779f6e57
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-03-17 13:31:38 -0400

    Update HelperMethods.java

--- HelperMethods.java
+++ HelperMethods.java
@@ -3,21 +3,56 @@ import java.util.Scanner;
 public class HelperMethods {
     public static void quadraticFormula(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
-        double firstRoot = (-b - (Math.sqrt(discriminant)))/(2*a);
-        double secondRoot = (-b + (Math.sqrt(discriminant)))/(2*a);
-        
+        double firstRoot = (-b - (Math.sqrt(discriminant))) / (2 * a);
+        double secondRoot = (-b + (Math.sqrt(discriminant))) / (2 * a);
+
         System.out.println("x = " + firstRoot + " and x = " + secondRoot + ".");
     }
 
-    public static double piApproximation(int n) {
+    public static double piApproximation(int n1) {
         double sum = 0.0;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n1; i++) {
             sum += Math.pow(-1, i) / (2 * i + 1);
         }
         return 4 * sum;
     }
-    
+
+    public static void asymptoteFinder(int m, int n2) {
+        switch (m - n2) {
+            case 1:
+                System.out.println("The asymptote is linear.");
+                break;
+            case 2:
+                System.out.println("The asymptote is quadratic.");
+                break;
+            case 3:
+                System.out.println("The asymptote is cubic.");
+                break;
+            case 4:
+                System.out.println("The asymptote is quartic.");
+                break;
+            case 5:
+                System.out.println("The asymptote is quintic.");
+                break;
+            case 6:
+                System.out.println("The asymptote is sextic.");
+                break;
+            case 7:
+                System.out.println("The asymptote is septic.");
+                break;
+            case 8:
+                System.out.println("The asymptote is octic.");
+                break;
+            case 9:
+                System.out.println("The asymptote is nonic.");
+                break;
+            case 10:
+                System.out.println("The asymptote is decic.");
+                break;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
 
@@ -41,14 +76,22 @@ public class HelperMethods {
                     break;
                 case 2:
                     System.out.println("Please enter an n value:");
-                    int n = keyboard.nextInt();
+                    int n1 = keyboard.nextInt();
                 
-                    System.out.println("Your pi approximation is: " + piApproximation(n));
+                    System.out.println("Your pi approximation is: " + piApproximation(n1));
+                    break;
+                case 3:
+                    System.out.println("Please enter an m value:");
+                    int m = keyboard.nextInt();
+                    System.out.println("Please enter an n value:");
+                    int n2 = keyboard.nextInt();
+
+                    asymptoteFinder(m, n2);
                     break;
                 case 4:
                     keyboard.close();
                     System.exit(1);
             }
         }
-        }
     }
+}
