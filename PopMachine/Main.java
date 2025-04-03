94f20159b1c992d73874935199370554d2643863
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-04-03 11:24:43 -0400

    Add files via upload

---
+++ PopMachine/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        PopMachine pop = new PopMachine(100, 10.0, 1.5);
+        
+        System.out.println("Please enter your money for soda:");
+        double money = keyboard.nextDouble();
+
+        pop.coinSlot(money);
+        pop.popButton(money);
+        
+        System.out.println("Your change is $" + pop.coinReturn());
+    }
+}
